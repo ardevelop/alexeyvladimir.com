f39b438a9c971f95c3f546da0d7b80d2509fb4dd
Vadim Solovei <vadims.olovei90@gmail.com>
2019-05-12 14:39:29 +0300

    add slider

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows: false,
+    loop: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    fadeSpeed: 1000
+  });
+});
